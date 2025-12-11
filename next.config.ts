0f0e2f44e53a37f9fbc7de8453c3aff004353f16
Aswath <165268917+aswath030304@users.noreply.github.com>
2025-12-11 22:44:35 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack(config) {
-    return config; // Enables Webpack automatically
+    return config;
+  },
+
+  experimental: {
+    typedRoutes: false,
+    serverActions: false,
+    tsconfig: false, 
   },
 };
 
